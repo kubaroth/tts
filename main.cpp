15f7b6a38f94af1a552daa3a743f55fec26342b5
kuba <kuba456@gmail.com>
2019-03-25 14:04:17 -0700

    Fix flickering on android when facused by falling back on the software rendering

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickWindow>
+#include <QSGRendererInterface>
 #include "tts.h"
 
 int main(int argc, char *argv[]) {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     qmlRegisterType<tts>("custom.tts", 1, 0, "TTS");
     
     QGuiApplication app(argc, argv);
-    
+    QQuickWindow::setSceneGraphBackend(QSGRendererInterface::Software);
+
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
